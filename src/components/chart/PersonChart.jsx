fd2abfdb6133a95d56844dc6ff9fb87674f18c99
Stéphane Vanberg <vanbergstephane@gmail.com>
2022-08-24 21:45:41 +0200

    Person chart style

--- src/components/chart/PersonChart.jsx
+++ src/components/chart/PersonChart.jsx
@@ -11,17 +11,18 @@ const PersonChart = ({ statistics }) => {
     }
 
     const datasets = getPersonChartDatasets(statistics.messagesPerPerson);
-    const personChart = buildPersonChart(canvasRef.current, datasets);
+    const labels = Object.keys(statistics.messagesPerPerson);
+    const personChart = buildPersonChart(canvasRef.current, datasets, labels);
     setChart(personChart);
   }, []);
 
   return (
-    <div className="mx-8 my-8 max-w-3xl">
+    <div className="mx-8 my-8 max-w-3xl flex flex-col">
       <h1 className="text-3xl tracking-tight font-bold text-gray-800 mb-2">
         Person chart
       </h1>
       <hr />
-      <div className="w-96">
+      <div className="w-80 self-center mt-4">
         <canvas ref={canvasRef}></canvas>
       </div>
     </div>
--- src/components/chart/personChartHelper.js
+++ src/components/chart/personChartHelper.js
@@ -1,24 +1,38 @@
 import Chart from "chart.js/auto";
 
-export const getPersonChartDatasets = (messagesPerPerson) => ([
+export const getPersonChartDatasets = (messagesPerPerson) => [
   {
     label: "Population (millions)",
-    // backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
     data: Object.values(messagesPerPerson),
   },
-])
+];
 
-export const buildPersonChart = (canvas, datasets) => {
+export const buildPersonChart = (canvas, datasets, labels) => {
   return new Chart(canvas, {
     type: "pie",
     data: {
-      // labels: Object.keys(statistics.messagesPerPerson),
+      labels,
       datasets,
     },
     options: {
-      title: {
-        display: true,
-        text: "Predicted world population (millions) in 2050",
+      backgroundColor: [
+        "rgba(126, 34, 206, 1)",
+        "rgba(126, 34, 206, 0.7)",
+        "rgba(126, 34, 206, 0.4)",
+        "rgba(126, 34, 206, 0.1)",
+      ],
+      plugins: {
+        legend: {
+          position: "bottom",
+          display: true,
+          onClick: (e) => e.native.stopPropagation(),
+        },
+        tooltip: {
+          displayColors: false,
+          callbacks: {
+            label: (tooltip) => tooltip.formattedValue + " messages",
+          },
+        },
       },
     },
   });
